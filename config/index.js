4312c066c9c565a2ea58e6eb764ee637b7c931e3
zacjjohnson <wecooked@gmail.com>
2022-12-17 16:19:51 -0500

    url changes

--- config/index.js
+++ config/index.js
@@ -27,10 +27,10 @@ module.exports = (app) => {
 
   // controls a very specific header to pass headers from the frontend
   app.use(
-    cors()
-    // cors({
-    //   origin: [FRONTEND_URL],
-    // })
+    
+    cors({
+      origin: [FRONTEND_URL],
+    })
   );
 
   // In development environment the app logs
